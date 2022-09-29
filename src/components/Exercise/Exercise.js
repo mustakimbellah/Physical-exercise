f4e2814ac257cfe1c2802b244ca45c0b79d121ab
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-09-29 12:44:15 +0600

    exercise part added

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -4,12 +4,17 @@ import './Exercise.css';
 const Exercise = (props) => {
     const { name, photo, time } = props.exercise;
     return (
-        <div className='exercise-container'>
+        <div className='exercise-container-r'>
             <img src={photo} alt="" srcset="" />
             <div className='exercise-info'>
                 <p className='exercise-name'>{name}</p>
-                <p>Time:{time}</p>
+                <p className='exercise-name2'>Time required: {time}s</p>
             </div>
+            <div className='btn'>
+                <button className='exercise-btn'>Add to list</button>
+            </div>
+
+
         </div>
     );
 };
